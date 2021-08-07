3bb3d559ad9415819d71a61708e66b7c5a05c2f2
Gevorg Tarkhanyan <gevorg.tarxanyan@icloud.com>
2021-08-07 23:19:15 +0400

    AmdCash

---
+++ AmdCash/LounchscreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LounchscreenViewController.swift
+//  AmdCash
+//
+//  Created by Gev on 04.08.21.
+//
+
+import UIKit
+
+class LounchscreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
